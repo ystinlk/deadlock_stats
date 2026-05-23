dd178ccd619eede639e265644d5abe92391615ca
ystinlk <k_vasnetsov@list.ru>
2026-05-23 16:00:05 +0700

    Добавил вывод героев\винрейт\пикрейт

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
 const assetsUrl = "https://assets.deadlock-api.com/v2/heroes"
 const statsUrl = "https://api.deadlock-api.com/v1/analytics/hero-stats"
+const table = document.getElementById("heroes-table")
 
 Promise.all([
     fetch(assetsUrl).then(r => r.json()),
@@ -8,5 +9,31 @@ Promise.all([
   ]).then(([heroes, stats]) => {
     console.log("Герои:", heroes)
     console.log("Статы:", stats)
-  })
+    
+    const allmatches = stats.reduce((summ, item) => summ + item.matches, 0);
 
+    heroes.forEach(hero => {
+
+    const heroStats = stats.find(stat => stat.hero_id === hero.id)
+    if (!heroStats) return
+
+    const winrate = (
+      heroStats.wins / heroStats.matches * 100
+    ).toFixed(1)
+
+
+
+    const PickRate = (heroStats.matches / allmatches * 100).toFixed(1)
+
+    const row = `
+      <tr>
+        <td>${hero.name}</td>
+        <td>${winrate}%</td>
+        <td>${PickRate}%</td>
+      </tr>
+    `
+
+    table.innerHTML += row
+
+  })
+})
