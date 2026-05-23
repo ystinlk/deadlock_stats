d037a07c4da1afa8cd3b90330a2de8637df86844
ystinlk <k_vasnetsov@list.ru>
2026-05-23 19:02:06 +0700

    123

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ Promise.all([
   fetch(assetsUrl).then(r => r.json()),
   fetch(statsUrl).then(r => r.json())
 ]).then(([heroes, stats]) => {
+  console.log("Герои:", heroes)
+  console.log("Статы:", stats)
   const allmatches = stats.reduce((summ, item) => summ + item.matches, 0); // общее колво матчей в этом патче
   let data_Sorted = []
 
